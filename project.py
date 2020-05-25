f47bc053c6b43cb2d9fd104c0b30df355338cfc8
DaoistDarvin <52687988+ArvindRajen@users.noreply.github.com>
2020-05-25 16:09:39 -0700

    Add files via upload

---
+++ project.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 00:34:50 2019
+
+@author: pnish
+"""
+
+'''import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data=np.load("olivetti_faces.npy")
+print("There are {} images in the dataset" .format(len(data)))'''
+
+#new
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+%matplotlib inline
+
+#import plotly.offline as py
+#py.init_notebook_mode(connected=True)
+#import plotly.graph_objs as go
+#import plotly.tools as tls
+
+##import warnings
+#warnings.filterwarnings('ignore')
+
+#import os
+#print(os.listdir("../input"))
+
+
+#LOADING DATASET
+pics = np.load("olivetti_faces.npy")
+labels = np.load("olivetti_faces_target.npy")
+print("pics: ", pics.shape)
+print("labels: ", labels.shape)
+
+fig = plt.figure(figsize=(20, 10))
+columns = 10
+rows = 4
+for i in range(1, columns*rows +1):
+    img = pics[10*(i-1),:,:]
+    fig.add_subplot(rows, columns, i)
+    plt.imshow(img, cmap = plt.get_cmap('gray'))
+    plt.title("person {}".format(i), fontsize=16)
+    plt.axis('off')
+    
+plt.suptitle("There are 40 distinct people in the dataset", fontsize=22)
+plt.show()
+
+Xdata = pics # store images in Xdata
+Ydata = labels.reshape(-1,1) # store labels in Ydata
+
+#SPLITTING OF DATASET
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(Xdata, Ydata, test_size = 0.2, random_state=46)
+
+print("x_train: ",x_train.shape)
+print("x_test: ",x_test.shape)
+print("y_train: ",y_train.shape)
+print("y_test: ",y_test.shape)
+
+#RESHAPE
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1]*x_train.shape[2])
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1]*x_test.shape[2])
+
+print("x_train: ",x_train.shape)
+print("x_test: ",x_test.shape)
+print("y_train: ",y_train.shape)
+print("y_test: ",y_test.shape)
+
+# Store accuracies of the machine learning methods for comparison at the end
+list_names = []
+list_accuracy = []
+
+#LOGISTIC
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression(max_iter=1000)
+lr.fit(x_train, y_train)
+LR_accuracy = round(lr.score(x_test, y_test)*100,2)
+
+print("LR_accuracy is %", LR_accuracy)
+
+list_names.append("Logistic Regression")
+list_accuracy.append(LR_accuracy)
+
+#RANDOM FOREST
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+rf = RandomForestClassifier(n_estimators = 400, random_state = 1)
+rf.fit(x_train, y_train)
+RF_accuracy = round(rf.score(x_test, y_test)*100,2)
+
+print("RF_accuracy is %", RF_accuracy)
+
+list_names.append("Random Forest")
+list_accuracy.append(RF_accuracy)
+
+#KNN
+from sklearn.neighbors import KNeighborsClassifier
+
+Knn = KNeighborsClassifier(n_neighbors = 1) # n_neighbors=1 gives the best result for this data
+Knn.fit(x_train, y_train)
+Knn_accuracy = round(Knn.score(x_test, y_test)*100,2)
+
+print("Knn_accuracy is %", Knn_accuracy)
+
+list_names.append("KNN")
+list_accuracy.append(Knn_accuracy)
+
+
+#CNN
+x_train = x_train.reshape(-1,64,64,1)
+x_test = x_test.reshape(-1,64,64,1)
+
+print("x_train: ",x_train.shape)
+print("x_test: ",x_test.shape)
+
+
+from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+
+model = Sequential()
+
+model.add(Conv2D(filters = 20, kernel_size = (5,5),padding = 'Same', 
+                 activation ='relu', input_shape = (64,64,1)))
+
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(filters = 50, kernel_size = (6,6),padding = 'Same', 
+                 activation ='relu'))
+
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(filters = 150, kernel_size = (5,5),padding = 'Same', 
+                 activation ='relu', input_shape = (64,64,1)))
+
+model.add(Flatten())
+model.add(Dense(256, activation = "relu"))
+model.add(Dropout(0.5))
+model.add(Dense(40, activation = "softmax"))
+
+optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
+
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.7, 
+                                            min_lr=0.00000000001)
+
+epoch = 37
+batch_size = 20
+
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=5,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.05, # Randomly zoom image 
+        width_shift_range=0,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+datagen.fit(x_train)
+
+history = model.fit_generator(
+                              datagen.flow(x_train,y_train_, batch_size=batch_size),
+                              epochs = epoch, 
+                              validation_data = (x_test,y_test_),
+                              verbose = 2, 
+                              steps_per_epoch=x_train.shape[0] // batch_size,
+                              callbacks=[learning_rate_reduction]
+                             )
